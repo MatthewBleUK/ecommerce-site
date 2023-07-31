710f41a4d7570ca96cc469d932872a79f628a276
MatthewBleUK <matthew@bleathman.com>
2023-08-01 01:49:03 +0300

    Add announcement bar component Adds the announcement bar component and renames the index.html's title.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
+import AnnouncementBar from "./components/AnnouncementBar";
+
 function App() {
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline text-blue-700">
-                Hello world!
-            </h1>
-        </>
-    );
+    return <AnnouncementBar />;
 }
 
 export default App;
---
+++ src/components/AnnouncementBar.jsx
@@ -0,0 +1,14 @@
+function AnnouncementBar() {
+    return (
+        <div
+            id="announcementBar"
+            className="h-12 flex items-center text-center"
+        >
+            <span className="grow text-white text-sm font-light">
+                Free Shipping to Bulgaria
+            </span>
+        </div>
+    );
+}
+
+export default AnnouncementBar;
