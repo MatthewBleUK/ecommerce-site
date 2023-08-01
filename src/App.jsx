b55d7287b315e2512945544c9049827dca46bc9b
MatthewBleUK <matthew@bleathman.com>
2023-08-01 15:30:30 +0300

    Add responsive header Adds the header navigation bar that collapse into mobile menu.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import AnnouncementBar from "./components/AnnouncementBar";
+import Header from "./components/Header";
 
 function App() {
-    return <AnnouncementBar title="Free Shipping to Bulgaria" />;
+    const navigationItems = ["Shoes", "Bags", "Dresses"];
+
+    return (
+        <>
+            <AnnouncementBar title="Free Shipping to Bulgaria" />
+            <Header nav={navigationItems} />
+            <div></div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+
+function App(props) {
+    // Sets state for active li item
+    const [activeContentIndex, setActiveContentIndex] = useState(0);
+    // Sets state for mobile hamburger menu
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <header className="bg-white border-b border-zinc-300">
+            {/* Logo */}
+            <div className="max-w-screen-2xl flex flex-wrap items-center justify-between mx-auto p-9">
+                <div className="">
+                    <img
+                        src="logo.png"
+                        className="w-100 h-10 mr-2"
+                        alt="Logo"
+                    />
+                </div>
+                {/* Mobile hamburger icon */}
+                <div className="block lg:hidden">
+                    <button
+                        onClick={() => setIsOpen(!isOpen)}
+                        className="flex items-center px-3 py-2 rounded text-black-500 hover:text-black-400"
+                    >
+                        <svg
+                            className={`fill-current h-6 w-6 ${
+                                isOpen ? "hidden" : "block"
+                            }`}
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                        </svg>
+                        <svg
+                            className={`fill-current h-6 w-6 ${
+                                isOpen ? "block" : "hidden"
+                            }`}
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+                        </svg>
+                    </button>
+                </div>
+
+                {/* navigation links */}
+                <div
+                    className={`w-full lg:flex lg:items-center lg:w-auto mt-4 lg:mt-0 ${
+                        isOpen ? "block" : "hidden"
+                    }`}
+                >
+                    <nav className="text-sm lg:flex-grow">
+                        <ul className="lg:flex">
+                            {props.nav.map((item, index) => (
+                                <li
+                                    key={item}
+                                    className="mt-4 lg:mt-0 lg:px-4"
+                                    onClick={() => setActiveContentIndex(index)}
+                                >
+                                    <a
+                                        href="#"
+                                        className={`hover:border-gray-700 border-b pb-1.5 ${
+                                            activeContentIndex === index
+                                                ? "border-gray-700"
+                                                : "border-transparent"
+                                        }`}
+                                    >
+                                        {item}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </nav>
+                </div>
+
+                {/* Right side cart, profile and search icons */}
+                <div id="header-icons" className="hidden lg:flex">
+                    <a
+                        href="#"
+                        className="px-4 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-300"
+                    >
+                        <svg width="24" height="24" viewBox="0 0 24 24">
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M18 9.5a7 7 0 1 1-14 0 7 7 0 0 1 14 0Zm-1.736 6.024a8 8 0 1 1 .71-.704l4.62 4.62a.5.5 0 1 1-.707.706l-4.623-4.622Z"
+                            ></path>
+                        </svg>
+                    </a>
+
+                    <a
+                        href="#"
+                        className="px-4 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-300"
+                    >
+                        <svg width="24" height="24" viewBox="0 0 24 24">
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M16 5.5a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm1 0a5 5 0 1 1-10 0 5 5 0 0 1 10 0Zm-12.5 14c0-2.143.486-3.732 1.596-4.796C7.212 13.634 9.058 13 12 13c2.942 0 4.788.635 5.904 1.704 1.11 1.064 1.596 2.652 1.596 4.796a.5.5 0 0 0 1 0c0-2.275-.514-4.186-1.904-5.518C17.212 12.656 15.058 12 12 12c-3.058 0-5.212.656-6.596 1.982C4.014 15.314 3.5 17.225 3.5 19.5a.5.5 0 0 0 1 0Z"
+                            ></path>
+                        </svg>
+                    </a>
+
+                    <a
+                        href="#"
+                        className="px-4 transition ease-in-out delay-75 hover:-translate-y-1 hover:scale-110 duration-300"
+                    >
+                        <svg width="24" height="24" viewBox="0 0 24 24">
+                            <circle cx="16.75" cy="19.949" r=".75"></circle>
+                            <circle cx="9.75" cy="19.949" r=".75"></circle>
+                            <path
+                                fillRule="evenodd"
+                                clipRule="evenodd"
+                                d="M18 18.2a.5.5 0 0 0 0-1h-7.99a2.5 2.5 0 0 1-2.46-2.06l-.123-.688h9.16a2.5 2.5 0 0 0 2.355-1.66l1.55-4.34a1.5 1.5 0 0 0-1.413-2.004H5.997l-.065-.364A3.5 3.5 0 0 0 2.488 3.2h-.99a.5.5 0 1 0 0 1h.99a2.5 2.5 0 0 1 2.46 2.06l1.617 9.057a3.5 3.5 0 0 0 3.446 2.884H18ZM6.176 7.45l12.903-.001a.5.5 0 0 1 .47.668l-1.548 4.34a1.5 1.5 0 0 1-1.413.996h-9.34L6.176 7.45Z"
+                            ></path>
+                        </svg>
+                    </a>
+                </div>
+            </div>
+        </header>
+    );
+}
+export default App;
